16289264fc6b77a857d531f8b77077332739b799
Atilio Emmanuel Ballesteros <atilio.emm.b@gmail.com>
2022-01-29 12:31:14 -0300

    Completed user.controller.ts and user-manager.controller.ts file methods

--- src/controllers/user-manager.controller.ts
+++ src/controllers/user-manager.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
+import { UserManagerService } from 'src/services/user-manager.service';
 
 @Controller('user-manager')
-export class UserManagerController {}
+export class UserManagerController {
+
+    constructor(private user : UserManagerService){}
+    
+    @Post()
+    createUser(@Body() body : any){
+        this.user.createUser(body);
+    }
+
+    @Get()
+    searchUser(){
+        this.user.listUser();
+    }
+    
+    @Put('id')
+    modifyUser(@Param('id') idUser: number, @Body() body: any){
+        this.modifyUser(idUser,body);
+    }
+
+    @Delete('id')
+    deleteUser(@Param('id') idUser: number){
+        this.user.deleteUser(idUser);
+    }
+}
--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put, Delete} from '@nestjs/common';
+import { UsersService } from 'src/services/users.service';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+
+    constructor(private userService : UsersService){}
+    
+    @Post()
+    createTask(@Body() body : any){
+        this.userService.createTask(body);
+    }
+
+    @Get()
+    listTasks(){
+        this.userService.listTask();
+    }
+    
+    @Put('id')
+    modifyTask(@Param('id') idTask: number, @Body() body: any){
+        this.userService.modifyTask(idTask,body);
+    }
+
+    @Delete('id')
+    deleteTask(@Param('id') idTask: number){
+        this.userService.deleteTask(idTask);
+    }
+
+}
