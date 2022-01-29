cc1084a79232ee4935df6ac2d7959532e7947ddf
Atilio Emmanuel Ballesteros <atilio.emm.b@gmail.com>
2022-01-28 21:21:19 -0300

    File creation: users.controller.ts, users.service.ts, tasks.controller.ts and tasks.services.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersController } from './users/users.controller';
+import { UsersService } from './users/users.service';
+import { TasksService } from './tasks/tasks.service';
+import { TasksController } from './tasks/tasks.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, UsersController, TasksController],
+  providers: [AppService, UsersService, TasksService],
 })
 export class AppModule {}
