d5f03a0fdf59354dc5c7eeec63554b8146088106
Atilio Emmanuel Ballesteros <atilio.emm.b@gmail.com>
2022-01-29 11:57:51 -0300

    Added the completeTask method of the Tasks class. Added methods of the UserService class

--- src/services/tasks.service.ts
+++ src/services/tasks.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Tasks } from 'src/entities/tasks.entity';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+    
+    constructor(@InjectRepository(Tasks) private taskRepository: Repository<Tasks>){}
+
+    async completeTask(idTask: number,body: any){
+        const aux = await this.taskRepository.findOne(idTask);
+        aux.status="C";
+        this.taskRepository.save(aux);
+        return "Task Complete"
+    }
+
+}
--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Users } from 'src/entities/users.entity';
+import { Tasks } from 'src/entities/tasks.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+    constructor(
+        @InjectRepository(Users) private userRepository: Repository<Users>,
+        @InjectRepository(Tasks) private taskRepository: Repository<Tasks>
+    ){}
+    
+
+    createTask(body :any) : string{
+        const newTask = this.taskRepository.create(body);
+        return "Task created successfully";
+    }
+
+    listTask(){
+        return this.taskRepository.find();
+    }
+
+    async completeTask(idTask: number,body: any) {
+        const aux = await this.taskRepository.findOne(idTask);
+        this.taskRepository.merge(aux,body);
+        return "Task modified successfully";
+    }
+
+    async deleteTask(idTask : number) {
+        this.taskRepository.delete(idTask);
+        return "Task deleted successfully";
+    }
+
+}
