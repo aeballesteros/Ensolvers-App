dd160a95ee69d4af3fe491a89b0281e56948f1b7
Atilio Emmanuel Ballesteros <atilio.emm.b@gmail.com>
2022-01-30 20:00:32 -0300

    script.sh

--- script.sh
+++ script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 
 sudo apt-get update
-clear
 sudo apt-get upgrade
-clear
 sudo apt-get install mysql-server
-sudo mysql -u rooot -p
-CREATE DATABASE mydb;
-GRANT USAGE ON mydb.* TO public@localhost IDENTIFIED BY '123456';
-QUIT
-clear
+sudo service mysql start
+sudo mysql -uroot -p  -e "CREATE DATABASE mydb";
+sudo mysql -uroot -p  -e "CREATE USER 'prueba'@'localhost' IDENTIFIED BY '123456'";
+sudo mysql -uroot -p  -e "GRANT ALL PRIVILEGES ON *.* TO 'prueba'@'localhost'";
+sudo mysql -uroot -p  -e "FLUSH PRIVILEGES";
+sudo apt-get install node
 sudo apt-get install nodejs
+sudo apt-get install npm
 sudo npm i -g @nestjs/cli
 sudo npm install --save @nestjs/typeorm typeorm mysql2
 sudo npm run typeorm -- migration:generate -init
 sudo npm run typeorm -- migration:run
+clear
 sudo npm run start:dev
-
 
